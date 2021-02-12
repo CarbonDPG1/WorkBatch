2ebb17ac8246621898f49d3bfe35e0273d5435a6
CarbonDPG1 <71844196+CarbonDPG1@users.noreply.github.com>
2021-02-12 19:25:12 -0400

    a
    a

---
+++ FB-CIDR.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+FILE=./FBRange.txt
+DATE=
+if test -f "$FILE"; then
+	echo "FB CIDR range already defined. Chjecking to see if file matches up to date list."
+	`whois -h whois.radb.net -- '-i origin AS32934' | grep -Eo "([0-9.]+){4}/[0-9]+" | head > T_FBRange.txt`
+	T_FILE=T_FBRange.txt
+	local MD5SUM=`md5sum $FILE`
+	local T_MD5SUM=`md5sum $T_FILE`
+	if [ $MD5SUM = $T_MD5SUM ]; then
+		echo "Files are identical. Not changing anything."
+	else
+		rm FBRange.txt
+		mv T_FBRange.txt FBRange.txt
+		touch Updated-`date +%F`
+	fi
+else
+	echo "FB CIDR range not defined. Creating the FB CIDR Range"
+	`whois -h whois.radb.net -- '-i origin AS32934' | grep -Eo "([0-9.]+){4}/[0-9]+" | head > FBRange.txt`
+fi	
